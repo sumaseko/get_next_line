95167ff1b45f7a3b1ffd8a7e0f1ed376983b3c83
Sunday MASEKO <sumaseko@c4r9s8.wethinkcode.co.za>
2019-06-28 16:46:22 +0200

    header_file

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumaseko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/28 16:33:21 by sumaseko          #+#    #+#             */
+/*   Updated: 2019/06/28 16:40:45 by sumaseko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE	32
+# define MAX_FD		2147483647
+
+# include <unistd.h>
+# include <libft/libft.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
